24000ea557d8375693016bba90e0ee8a889734d7
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-11-13 11:47:40 +0300

    выполнил задание 1.3 нашел изолированные, концевые и доминирующие вершины.

--- l_5.cpp
+++ l_5.cpp
@@ -25,6 +25,14 @@ public:
         delete[] matrix;
     }
 
+    void set_matrix(int** mat) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = mat[i][j];
+            }
+        }
+    }
+
     void generate_adjacency_matrix() {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -63,18 +71,119 @@ public:
         //}
         //return size;
     }
+
+    void find_isolated_vertices() {
+        cout << "Изолированные вершины: ";
+        for (int i = 0; i < n; ++i) {
+            bool isolated = true;
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] != 0 || matrix[j][i] != 0) {
+                    isolated = false;
+                    break;
+                }
+            }
+            if (isolated) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    void find_terminal_vertices() {
+        cout << "Концевые вершины: ";
+        for (int i = 0; i < n; ++i) {
+            int degree = 0;
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] != 0 || matrix[j][i] != 0) {
+                    degree++;
+                }
+            }
+            if (degree == 1) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    void find_dominating_vertices() {
+        cout << "Доминирующие вершины: ";
+        for (int i = 0; i < n; ++i) {
+            bool dominating = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && (matrix[i][j] == 0 && matrix[j][i] == 0)) {
+                    dominating = false;
+                    break;
+                }
+            }
+            if (dominating) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
 };
 
-int main() {
-    setlocale(LC_ALL, "");
-    srand(time(0));
+    int main() {
+        setlocale(LC_ALL, "");
 
-    int n = 10;
-    Graph graph(n);
+        int n = 4;
 
-    graph.print_matrix();
+        // Пример матрицы с изолированными вершинами
+        int** isolated_matrix = new int* [n];
+        for (int i = 0; i < n; ++i) {
+            isolated_matrix[i] = new int[n];
+            for (int j = 0; j < n; ++j) {
+                isolated_matrix[i][j] = 0;
+            }
+        }
 
-    cout << "Размер графа (количество вершин): " << graph.get_graph_size() << endl;
+        // Пример матрицы с концевыми вершинами
+        int** terminal_matrix = new int* [n];
+        for (int i = 0; i < n; ++i) {
+            terminal_matrix[i] = new int[n];
+            for (int j = 0; j < n; ++j) {
+                terminal_matrix[i][j] = 0;
+            }
+        }
+        terminal_matrix[0][1] = 1;
+        terminal_matrix[1][0] = 1;
+        terminal_matrix[2][3] = 1;
+        terminal_matrix[3][2] = 1;
 
-    return 0;
-}
+        // Пример матрицы с доминирующей вершиной
+        int** dominating_matrix = new int* [n];
+        for (int i = 0; i < n; ++i) {
+            dominating_matrix[i] = new int[n];
+            for (int j = 0; j < n; ++j) {
+                dominating_matrix[i][j] = 1;
+            }
+            dominating_matrix[i][i] = 0;
+        }
+
+        Graph isolated_graph(n);
+        isolated_graph.set_matrix(isolated_matrix);
+        isolated_graph.print_matrix();
+        isolated_graph.find_isolated_vertices();
+
+        Graph terminal_graph(n);
+        terminal_graph.set_matrix(terminal_matrix);
+        terminal_graph.print_matrix();
+        terminal_graph.find_terminal_vertices();
+
+        Graph dominating_graph(n);
+        dominating_graph.set_matrix(dominating_matrix);
+        dominating_graph.print_matrix();
+        dominating_graph.find_dominating_vertices();
+
+        // Освобождение памяти
+        for (int i = 0; i < n; ++i) {
+            delete[] isolated_matrix[i];
+            delete[] terminal_matrix[i];
+            delete[] dominating_matrix[i];
+        }
+        delete[] isolated_matrix;
+        delete[] terminal_matrix;
+        delete[] dominating_matrix;
+
+        return 0;
+    }
